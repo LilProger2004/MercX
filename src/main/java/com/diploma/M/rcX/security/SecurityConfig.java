d7996241f60a8eec04918e64a0c029ca7e49c3b8
Vladimir <djeksaly@gmail.com>
2025-04-11 19:01:58 +0300

    fix SecurityConfig

--- src/main/java/com/diploma/M/rcX/security/SecurityConfig.java
+++ src/main/java/com/diploma/M/rcX/security/SecurityConfig.java
@@ -6,8 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
-import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,17 +18,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
+                        .requestMatchers("/user/me").authenticated()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2 -> oauth2
-                .loginPage("/oauth2/authorization/mercx-realm")
-                  .redirectionEndpoint(Customizer.withDefaults())
-                     .failureUrl("/login/oauth2/code/*")
-        )// OAuth2 login с настройками по умолчанию
+                .oauth2Login(httpSecurityOAuth2LoginConfigurer -> httpSecurityOAuth2LoginConfigurer
+                        //.defaultSuccessUrl("/public/success")
+                        .failureUrl("/public/fail")) // OAuth2 login с настройками по умолчанию
                 .oauth2Client(Customizer.withDefaults()) // OAuth2 клиентская поддержка
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
@@ -35,7 +35,7 @@ public class SecurityConfig {
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .logout(logout -> logout
-                        .logoutSuccessUrl("https://localhost:8085/realms/mercx-realm/protocol/openid-connect/logout?redirect_uri=https://localhost:8081/unauthenticated")
+                        .logoutSuccessUrl("http://localhost:8085/realms/mercx-realm/protocol/openid-connect/logout?redirect_uri=http://localhost:8081/unauthenticated")
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
                 );
@@ -45,15 +45,15 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");  // Префикс для ролей
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access.roles");  // Где Keycloak хранит роли
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("realm_access.roles");
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
 
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-
-        return jwtConverter;
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return converter;
     }
+
 
     @Bean
     public JwtDecoder jwtDecoder() {
@@ -66,5 +66,6 @@ public class SecurityConfig {
 
         return decoder;
     }
+
 }
 
